cc8a6df8bf4c3e4afae9d13a87b9e8b2df568c5b
ivandejanovic <idejanovic@gmail.com>
2014-11-25 22:49:39 +0100

    Updated licence. Updated controller package.

--- src/main/java/com/quine/qwak/controller/AbstractQwakJqGridControllerWithICEPush.java
+++ src/main/java/com/quine/qwak/controller/AbstractQwakJqGridControllerWithICEPush.java
@@ -1,27 +1,27 @@
 /**
  * The MIT License (MIT)
- * 
- * Copyright 2008-2013 Quine Interactive and other contributors
- * www.quineinteractive.com
- * 
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- * 
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Copyright 2008-2014 Ivan Dejanovic and Quine Interactive
+* www.quineinteractive.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  */
 package com.quine.qwak.controller;
@@ -41,73 +41,71 @@ import com.quine.qwak.util.jqgrid.QwakJqGridUniqueStringId;
 
 /**
  * AbstractQwakJqGridControllerWithICEPush class extends
- * AbstractQwakJqGridController with ICEpush support.
+ * AbstractQwakJqGridController an adds ICEpush support.
  * 
  * @author Ivan Dejanovic
  * 
  * @version 1.0
+ * 
+ * @since 1.0
  */
 
 abstract public class AbstractQwakJqGridControllerWithICEPush<T extends QwakJqGridUniqueId, V extends QwakJqGridUniqueStringId, U>
-		extends AbstractQwakJqGridController<T, V, U> {
-	@RequestMapping(params = "add")
-	public @ResponseBody
-	QwakJSONStatusResponse add(HttpServletRequest request,
-			HttpServletResponse response, @RequestParam("add") String add,
-			@RequestParam("gridname") String gridName) throws Exception {
-		QwakJSONStatusResponse status = super.add(request, response, add,
-				gridName);
+        extends AbstractQwakJqGridController<T, V, U> {
+    @RequestMapping(params = "add")
+    public @ResponseBody
+    QwakJSONStatusResponse add(HttpServletRequest request,
+                               HttpServletResponse response,
+                               @RequestParam("add") String add,
+                               @RequestParam("gridname") String gridName) throws Exception {
+        QwakJSONStatusResponse status = super.add(request, response, add, gridName);
 
-		if (status.getSuccess()) {
-			PushContext pushContext = PushContext.getInstance(request
-					.getServletContext());
-			pushContext.push(getPushTopic());
-		}
+        if (status.getSuccess()) {
+            PushContext pushContext = PushContext.getInstance(request.getServletContext());
+            pushContext.push(getPushTopic());
+        }
 
-		return status;
-	}
+        return status;
+    }
 
-	@RequestMapping(params = "edit")
-	public @ResponseBody
-	QwakJSONStatusResponse edit(HttpServletRequest request,
-			HttpServletResponse response, @RequestParam("edit") String edit,
-			@RequestParam("gridname") String gridName,
-			@RequestParam("id") String id) throws Exception {
-		QwakJSONStatusResponse status = super.edit(request, response, edit,
-				gridName, id);
+    @RequestMapping(params = "edit")
+    public @ResponseBody
+    QwakJSONStatusResponse edit(HttpServletRequest request,
+                                HttpServletResponse response,
+                                @RequestParam("edit") String edit,
+                                @RequestParam("gridname") String gridName,
+                                @RequestParam("id") String id) throws Exception {
+        QwakJSONStatusResponse status = super.edit(request, response, edit, gridName, id);
 
-		if (status.getSuccess()) {
-			PushContext pushContext = PushContext.getInstance(request
-					.getServletContext());
-			pushContext.push(getPushTopic());
-		}
+        if (status.getSuccess()) {
+            PushContext pushContext = PushContext.getInstance(request.getServletContext());
+            pushContext.push(getPushTopic());
+        }
 
-		return status;
-	}
+        return status;
+    }
 
-	@RequestMapping(params = "delete")
-	public @ResponseBody
-	QwakJSONStatusResponse delete(HttpServletRequest request,
-			HttpServletResponse response,
-			@RequestParam("delete") String delete,
-			@RequestParam("gridname") String gridName,
-			@RequestParam("id") String id) throws Exception {
-		QwakJSONStatusResponse status = super.delete(request, response, delete,
-				gridName, id);
+    @RequestMapping(params = "delete")
+    public @ResponseBody
+    QwakJSONStatusResponse delete(HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  @RequestParam("delete") String delete,
+                                  @RequestParam("gridname") String gridName,
+                                  @RequestParam("id") String id) throws Exception {
+        QwakJSONStatusResponse status = super.delete(request, response, delete,	gridName, id);
 
-		if (status.getSuccess()) {
-			PushContext pushContext = PushContext.getInstance(request
-					.getServletContext());
-			pushContext.push(getPushTopic());
-		}
+        if (status.getSuccess()) {
+            PushContext pushContext = PushContext.getInstance(request.getServletContext());
+            pushContext.push(getPushTopic());
+        }
 
-		return status;
-	}
+        return status;
+    }
 
-	/**
-	 * Creates an instance of the data object used in the controller.
-	 * 
-	 * @return pushTopic
-	 */
-	abstract protected String getPushTopic();
+    /**
+     * Creates an instance of the data object used in the controller.
+     * 
+     * @return pushTopic
+     */
+    abstract protected String getPushTopic();
 }
